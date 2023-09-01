ba55b7a32e537a9c920a9922afe5eebf7f1268ed
Obua Denis Daniel <dd.obua@gmail.com>
2023-09-01 09:32:33 +0300

    Create task list

--- src/index.js
+++ src/index.js
@@ -1,1 +1,11 @@
 import './styles.css';
+
+const list = [
+  { description: 'Pray', completed: true, index: 0 },
+  { description: 'Brush', completed: true, index: 1 },
+  { description: 'Bathe', completed: true, index: 2 },
+  { description: 'Walk', completed: false, index: 3 },
+  { description: 'Sing', completed: false, index: 4 },
+  { description: 'Treck', completed: false, index: 5 },
+  { description: 'Work', completed: false, index: 6 },
+];
