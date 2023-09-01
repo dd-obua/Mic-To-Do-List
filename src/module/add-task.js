affbe2124e4ecbad42172ecc3103cb38a478c7f5
Obua Denis Daniel <dd.obua@gmail.com>
2023-09-01 17:14:46 +0300

    Give ids to task input field & submit button

--- src/module/add-task.js
+++ src/module/add-task.js
@@ -1,1 +1,14 @@
-const addTask = (taskList, task) => taskList.push(task);
+const addTask = task => {
+  // Check if data already exists in local storage
+  let storedTaskList = JSON.parse(localStorage.getItem('taskList')) || [];
+
+  // Ensure existing data is an array
+  if (!Array.isArray(storedTaskList)) storedTaskList = [];
+
+  // Add task to task list
+  storedTaskList.push(task);
+
+  // Update local storage
+  localStorage.setItem('taskList', JSON.stringify(storedTaskList));
+};
+
