6abe6ee4c4d2a7888330818f353bb34c02a18dcb
Obua Denis Daniel <dd.obua@gmail.com>
2023-09-01 11:11:35 +0300

    Update list array

--- src/index.js
+++ src/index.js
@@ -3,11 +3,9 @@ import './styles.css';
 const list = [
   { description: 'Pray', completed: true, index: 0 },
   { description: 'Brush', completed: true, index: 1 },
-  { description: 'Bathe', completed: true, index: 2 },
-  { description: 'Walk', completed: false, index: 3 },
-  { description: 'Sing', completed: false, index: 4 },
-  { description: 'Treck', completed: false, index: 5 },
-  { description: 'Work', completed: false, index: 6 },
+  { description: 'Walk', completed: false, index: 2 },
+  { description: 'Sing', completed: false, index: 3 },
+  { description: 'Treck', completed: false, index: 4 },
 ];
 
 const listContainer = document.querySelector('ul');
