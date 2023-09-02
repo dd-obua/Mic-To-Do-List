76b73edd06c376b4885651f817b08947b6975c75
Obua Denis Daniel <dd.obua@gmail.com>
2023-09-02 17:08:56 +0300

    Refactor show task list function

--- src/modules/show-task.list.js
+++ src/modules/show-task.list.js
@@ -58,85 +58,30 @@ export const showTaskList = taskList => {
   const listContainer = document.querySelector('ul');
 
   taskList.forEach(item => {
-    // Create HTML elements
-    const listItem = document.createElement('li');
+    const listItem = createListItem(item);
+    listItem.index = item.index;
 
-    const checkbox = document.createElement('input');
-    checkbox.type = 'checkbox';
-    const taskParagraph = document.createElement('p');
-    taskParagraph.textContent = item.description;
-    const modificationParagraph = document.createElement('p');
-    const optionsIcon = document.createElement('i');
-    optionsIcon.classList.add('fa', 'fa-ellipsis-v', 'icon');
-    const optionsSpan = document.createElement('span');
-    optionsSpan.classList.add('options-span');
-    const deleteIcon = document.createElement('i');
-    deleteIcon.classList.add('fa', 'fa-trash', 'icon');
-    optionsSpan.appendChild(optionsIcon);
-    const deleteSpan = document.createElement('span');
-    deleteSpan.classList.add('delete-span');
-    deleteSpan.appendChild(deleteIcon);
-
-    modificationParagraph.appendChild(optionsSpan);
-    modificationParagraph.appendChild(deleteSpan);
-
-    // Place child elements inside parent ones
-    listItem.appendChild(checkbox);
-    listItem.appendChild(taskParagraph);
-    listItem.appendChild(modificationParagraph);
-    listContainer.appendChild(listItem);
-
-    // Style list components
-    listItem.classList.add('list-display');
-    modificationParagraph.classList.add('manipulate');
-    deleteSpan.classList.add('hidden');
-
-    // Manipulate tasks
     listItem.addEventListener('click', () => {
-      // Check if another item was selected and toggle its visibility
       if (selectedItem) {
-        const optionsSpan = selectedItem.querySelector('.options-span');
-        const deleteSpan = selectedItem.querySelector('.delete-span');
-        if (optionsSpan && deleteSpan) {
-          optionsSpan.classList.remove('hidden');
-          deleteSpan.classList.add('hidden');
-        }
+        const [optionsSpan, deleteSpan] = selectedItem.querySelectorAll(
+          '.options-span,.delete-span'
+        );
+        optionsSpan.classList.remove('hidden');
+        deleteSpan.classList.add('hidden');
       }
 
-      // Remove options span and display delete span if a list item is clicked
-      const optionsSpan = listItem.querySelector('.options-span');
-      const deleteSpan = listItem.querySelector('.delete-span');
-
-      if (optionsSpan && deleteSpan) {
-        optionsSpan.classList.add('hidden');
-        deleteSpan.classList.remove('hidden');
-      }
-
-      // Update currently selected item
+      const [optionsSpan, deleteSpan] = listItem.querySelectorAll(
+        '.options-span,.delete-span'
+      );
+      optionsSpan.classList.add('hidden');
+      deleteSpan.classList.remove('hidden');
       selectedItem = listItem;
     });
 
-    // Remove item from local storage
-    deleteSpan.addEventListener('click', () => {
-      // Create an id from the task index
-      const taskId = item.index;
-
-      // Delete selected task
-      const updatedTaskList = taskList.filter(task => task.index !== taskId);
-
-      // Re-index items to ensure consecutive indexes
-      updatedTaskList.forEach((task, i) => {
-        task.index = i + 1;
-      });
-
-      // Update local storage
-      localStorage.setItem('taskList', JSON.stringify(updatedTaskList));
-
-      // Remove the list item from display
-      listContainer.removeChild(listItem);
-
-      // Remove deleted item from display
-      selectedItem = null;
+    listItem.querySelector('.delete-span').addEventListener('click', () => {
+      updateTaskListAndDisplay(taskList, listItem, listContainer);
     });
+
+    listContainer.appendChild(listItem);
   });
 };
